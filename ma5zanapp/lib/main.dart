5aa107189f687855e16c4e3b2ec2a29d95766217
AKKanoon <abdulrahmankanoon@gmail.com>
2024-01-05 23:11:58 +0200

    changed title name

--- ma5zanapp/lib/main.dart
+++ ma5zanapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'this is ma5zan store',
       theme: ThemeData(
         // This is the theme of your application.
         //
